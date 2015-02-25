377890b62dea9e52d11c20fdf9760143a6e380b0
erick <erick@autoweb.com>
2015-02-25 15:47:27 -0600

    tiene funcionalidad basica, icompleto

--- rankhospital.R
+++ rankhospital.R
@@ -1,13 +1,34 @@
 rankhospital <- function(state, outcome, num = "best"){
   source("utilities.R")
-  ## Read outcome data
+  ##Read outcome data
     hospitalData = read.csv("hospital-data.csv")
-    outcomeData = read.csv("outcome-of-care-measures.csv")}
+    outcomeData = read.csv("outcome-of-care-measures.csv")
 
-  ## Check that state and outcome are valid
+  ##Check that state and outcome are valid
     checkState(state, hospitalData)
     checkOutcome(outcome) 
   
   ##Return hospital name in that state with te given rank 30-day death rate
+    index <- getIndexTable(outcome)
+    raw <- as.numeric(as.character(outcomeData[,index]))
+    orderValues <- unique(sort(raw, decreasing = FALSE))
+
+    value <- orderValues[ valNum(num,orderValues) ] 
   
+    if( !is.na( value ) ){
+      print(value)##DEBUG
+    }else{
+      NA
+    }
+      
+  
+}
+
+valNum <- function(num, orderValues){
+  
+  if( is.character(num) ){
+    if( num == "worst" ) num <- length(orderValues)
+    else num <- 1  
+  }
+  num
 }
