e57c597652da7707d0203eaa2624f40de3c0e91d
erick <erick@autoweb.com>
2015-02-25 11:52:24 -0600

    completo

--- best.R
+++ best.R
@@ -1,38 +1,60 @@
-best <- function(state, outcome) {
+best <- function(state = "", outcome = "") {
   ## Read outcome data
-  hospitalData = read.csv("hospital-data.csv")
-  outcomeData = read.csv("outcome-of-care-measures.csv")
+    hospitalData = read.csv("hospital-data.csv")
+    outcomeData = read.csv("outcome-of-care-measures.csv")
   
   ## Check that state and outcome are valid
-  if( !checkState(state, hospitalData) ){
-    stop("invalid state")
-  }
-  if( !checkOutcome(outcome) ){
-    stop("invalid outcome")
-  }
+    if( !checkState(state, hospitalData) ){
+      stop("invalid state")
+    }
+    if( !checkOutcome(outcome) ){
+      stop("invalid outcome")
+    }
   
-  print("all pass")
   ##Return hospital name in that state with lowest 30-day death rate
+    ##obteniendo los hospitales por estado
+    hospitales <- getHostipalByState(state, outcomeData)
+    min <-getMinOutcome(outcome,hospitales)
+    print(getHospitalByMin(outcome,min, hospitales))
   
 }
 
+##Valida el parametro state de la funcion best
 checkState <- function(state, hospitalData){
   states <- getStates(hospitalData)
   state %in% states
 }
 
+##Valida el parametro outcome de la funcion best
 checkOutcome <- function(outcome){
   validOutcome <- c("heart failure", "heart attack", "pneumonia")
   outcome %in% validOutcome
 }
 
+## Retorna los estados
 getStates <- function(rawData){
   unique(rawData[,7])
 }
 
 getHostipalByState <- function(state, hospitalData){
-  
+  subset( hospitalData,State == state )
 }
 
+getIndexTable <- function(outcome){
+  v <- c(19,13,25)
+  names(v) <- c("heart failure", "heart attack", "pneumonia")   
+  v[outcome]
+}
 
+getMinOutcome <- function(outcome, hospitales){  
+   index <- getIndexTable(outcome)
+   raw <- as.numeric(as.character(hospitales[,index]))
+   values <- raw[!is.na(raw)]
+   min(values)
+}
 
+getHospitalByMin <- function(outcome,value, hospitales){
+  index <- getIndexTable(outcome)
+  ##hospitales[,index] == value
+  subset(hospitales,hospitales[,index] == value, select = 2 )
+}
