9273d9bafe28b504bf16976b76ddf7283a6d252d
erick <erick@autoweb.com>
2015-02-26 09:05:18 -0600

    Creacion archivo parte 4

---
+++ rankall.R
@@ -0,0 +1,11 @@
+rankall <- function(outcome, num = "best"){
+  ##Read outcome data
+  hospitalData = read.csv("hospital-data.csv")
+  outcomeData = read.csv("outcome-of-care-measures.csv")
+  
+  ##Check that state and outcome are valid
+  
+  ##For each state, find the hospital of the giben rank
+  
+  ##Return a data frame with the hospital names and the abbreviated state name
+}
